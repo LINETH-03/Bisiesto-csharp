a969953e47100dd9ea6e3b63228b68a92e3a2675
LINETH-03 <lperezh9@miumg.edu.gt>
2021-03-13 15:47:57 -0600

    FIRST COMMIT

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace examen_programacion_I
+{
+    class Program
+    {
+        static int año;
+        static void Main(string[] args)
+        {
+            Console.WriteLine("INGRESE AÑO");
+            año = int.Parse(Console.ReadLine());
+            funcionalidad();
+
+        }
+        
+        static void funcionalidad(){
+            if(año % 4 == 0 && año % 100 !=0 || año % 400==0){
+                Console.WriteLine(año + " ES BISIESTO");
+            }
+            else
+            {
+                Console.WriteLine(año + " NO ES BISIESTO");
+            }
+
+        }
+    }
+}
